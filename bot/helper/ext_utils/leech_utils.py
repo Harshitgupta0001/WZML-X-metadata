19d60bd861a3b564617e12682c86abc98dab88e7
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-07-27 12:41:36 +0530

    Update leech_utils.py
    Signed-off-by: Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -12,7 +12,7 @@ from asyncio.subprocess import PIPE
 from telegraph import upload_file
 from langcodes import Language
 
-from bot import LOGGER, MAX_SPLIT_SIZE, config_dict, user_data
+from bot import bot_cache, LOGGER, MAX_SPLIT_SIZE, config_dict, user_data
 from bot.modules.mediainfo import parseinfo
 from bot.helper.ext_utils.bot_utils import cmd_exec, sync_to_async, get_readable_file_size, get_readable_time
 from bot.helper.ext_utils.fs_utils import ARCH_EXT, get_mime_type
@@ -77,7 +77,13 @@ async def get_media_info(path, metadata=False):
     tags = fields.get('tags', {})
     artist = tags.get('artist') or tags.get('ARTIST') or tags.get("Artist")
     title = tags.get('title') or tags.get('TITLE') or tags.get("Title")
-    return duration, artist, title
+    movie_name = tags.get('MOVIE name') or tags.get('Movie Name') or tags.get('Movie Name') or tags.get('MOVIE NAME') or tags.get("movie name")
+    comment = tags.get('comment') or tags.get('COMMENT') or tags.get("Comment")
+    copyright = tags.get('COPYRIGHT') or tags.get('copyright') or tags.get("Copyright")
+    author = tags.get('AUTHOR') or tags.get('author') or tags.get("Author")
+    website = tags.get('WEBSITE') or tags.get('Website') or tags.get('WebSite') or tags.get("website")
+    encoded = tags.get("Encoded by")
+    return duration, artist, title, movie_name, comment, copyright, author, website, encoded
 
 
 async def get_document_type(path):
@@ -116,7 +122,7 @@ async def get_audio_thumb(audio_file):
     if not await aiopath.exists(des_dir):
         await mkdir(des_dir)
     des_dir = ospath.join(des_dir, f"{time()}.jpg")
-    cmd = ["ffmpeg", "-hide_banner", "-loglevel", "error",
+    cmd = [bot_cache['pkgs'][2], "-hide_banner", "-loglevel", "error",
            "-i", audio_file, "-an", "-vcodec", "copy", des_dir]
     status = await create_subprocess_exec(*cmd, stderr=PIPE)
     if await status.wait() != 0 or not await aiopath.exists(des_dir):
@@ -135,7 +141,7 @@ async def take_ss(video_file, duration=None, total=1, gen_ss=False):
     if duration == 0:
         duration = 3
     duration = duration - (duration * 2 / 100)
-    cmd = ["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", "",
+    cmd = [bot_cache['pkgs'][2], "-hide_banner", "-loglevel", "error", "-ss", "",
            "-i", video_file, "-vf", "thumbnail", "-frames:v", "1", des_dir]
     tstamps = {}
     thumb_sem = Semaphore(3)
@@ -183,7 +189,7 @@ async def split_file(path, size, file_, dirpath, split_size, listener, start_tim
         while i <= parts or start_time < duration - 4:
             parted_name = f"{base_name}.part{i:03}{extension}"
             out_path = ospath.join(dirpath, parted_name)
-            cmd = ["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", str(start_time), "-i", path,
+            cmd = [bot_cache['pkgs'][2], "-hide_banner", "-loglevel", "error", "-ss", str(start_time), "-i", path,
                    "-fs", str(split_size), "-map", "0", "-map_chapters", "-1", "-async", "1", "-strict",
                    "-2", "-c", "copy", out_path]
             if not multi_streams:
